14d71ecdd08ced18724386254b5fc6ba80d8d2c6
Uladzimirant <vladimir.bwmail@gmail.com>
2022-10-12 22:25:16 +0300

    Add main program cycle and a lot of functions

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace Lesson3
 {
+    //exception class for messages, cycle should restart on its catch
     class ExpectedException : Exception
     {
         public ExpectedException(string? message) : base(message)
@@ -13,11 +14,12 @@ namespace Lesson3
         {
         }
     }
+    //info for operators and functions
     internal struct OpInfo
     {
-        public byte Level;
-        public bool IsLeft;
-        public byte RequiredAmount;
+        public byte Level; //Priority of operation
+        public bool IsLeft; //Is operation left assosiated
+        public byte RequiredAmount; //1 - Unary, 2 - Binary 
         public Func<double[], double> Func;
         public bool IsFunction;
 
@@ -32,15 +34,42 @@ namespace Lesson3
     }
     public class Program
     {
+        //list of operators and functions, their info and lambdas
         static Dictionary<string, OpInfo> opInfo = new Dictionary<string, OpInfo>()
         {
-            { "_" , new OpInfo(1, true, 1, (args) => { return -args[0]; }) },
+            { "_" , new OpInfo(9, false, 1, (args) => { return -args[0]; }) },
             { "+", new OpInfo(2, true, 2, (args) => { return args[0] + args[1]; }) },
             { "-", new OpInfo(2, true, 2, (args) => { return args[0] - args[1]; }) },
             { "*", new OpInfo(3, true, 2, (args) => { return args[0] * args[1]; }) },
             { "/", new OpInfo(3, true, 2, (args) => { return args[0] / args[1]; }) },
+            { "%", new OpInfo(3, true, 2, (args) => { return args[0] % args[1]; }) },
             { "^", new OpInfo(4, false, 2, (args) => { return Math.Pow(args[0], args[1]); }) },
-            { "abs", new OpInfo(1, true, 1, (args) => {return Math.Abs(args[0]); }, true) }
+            { "abs", new OpInfo(1, true, 1, (args) => {return Math.Abs(args[0]); }, true) },
+            { "sin", new OpInfo(1, true, 1, (args) => {return Math.Sin(args[0]); }, true) },
+            { "cos", new OpInfo(1, true, 1, (args) => {return Math.Cos(args[0]); }, true) },
+            { "tan", new OpInfo(1, true, 1, (args) => {return Math.Tan(args[0]); }, true) },
+            { "ctg", new OpInfo(1, true, 1, (args) => {return 1.0/Math.Tan(args[0]); }, true) },
+            { "asin", new OpInfo(1, true, 1, (args) => {return Math.Asin(args[0]); }, true) },
+            { "acos", new OpInfo(1, true, 1, (args) => {return Math.Acos(args[0]); }, true) },
+            { "atan", new OpInfo(1, true, 1, (args) => {return Math.Atan(args[0]); }, true) },
+            { "actg", new OpInfo(1, true, 1, (args) => {return Math.PI/2.0 - Math.Atan(args[0]); }, true) },
+            { "sinh", new OpInfo(1, true, 1, (args) => {return Math.Sinh(args[0]); }, true) },
+            { "cosh", new OpInfo(1, true, 1, (args) => {return Math.Cosh(args[0]); }, true) },
+            { "tanh", new OpInfo(1, true, 1, (args) => {return Math.Tanh(args[0]); }, true) },
+            { "ctgh", new OpInfo(1, true, 1, (args) => {return 1.0/Math.Tanh(args[0]); }, true) },
+            { "asinh", new OpInfo(1, true, 1, (args) => {return Math.Asinh(args[0]); }, true) },
+            { "acosh", new OpInfo(1, true, 1, (args) => {return Math.Acosh(args[0]); }, true) },
+            { "atanh", new OpInfo(1, true, 1, (args) => {return Math.Atanh(args[0]); }, true) },
+            { "sqrt", new OpInfo(1, true, 1, (args) => {return Math.Sqrt(args[0]); }, true) },
+            { "cbrt", new OpInfo(1, true, 1, (args) => {return Math.Cbrt(args[0]); }, true) },
+            { "exp", new OpInfo(1, true, 1, (args) => {return Math.Exp(args[0]); }, true) },
+            { "ceiling", new OpInfo(1, true, 1, (args) => {return Math.Ceiling(args[0]); }, true) },
+            { "floor", new OpInfo(1, true, 1, (args) => {return Math.Floor(args[0]); }, true) },
+            { "round", new OpInfo(1, true, 1, (args) => {return Math.Round(args[0]); }, true) },
+            { "log", new OpInfo(1, true, 1, (args) => {return Math.Log(args[0]); }, true) },
+            { "logtwo", new OpInfo(1, true, 1, (args) => {return Math.Log2(args[0]); }, true) },
+            { "logten", new OpInfo(1, true, 1, (args) => {return Math.Log10(args[0]); }, true) },
+            { "sign", new OpInfo(1, true, 1, (args) => {return Math.Sign(args[0]); }, true) },
         };
         static HashSet<string> functions = opInfo.Where(e => e.Value.IsFunction).Select(e => e.Key).ToHashSet();
         static string operators = String.Join("\\", opInfo.Where(e => !e.Value.IsFunction).Select(e => e.Key));
@@ -59,21 +88,25 @@ namespace Lesson3
                 case "(": return RPNType.LBRACKET;
                 case ")": return RPNType.RBRACKET;
                 default:
-                    return opInfo.ContainsKey(s) ? (opInfo[s].Level < 0 ? RPNType.FUNCTION : RPNType.OPERATION) : RPNType.OPERAND;
+                    return opInfo.ContainsKey(s) ? (opInfo[s].IsFunction ? RPNType.FUNCTION : RPNType.OPERATION) : RPNType.OPERAND;
             }
         }
+
+        //Converts our expression to Reverse Polish Notation by Shunting yard algorithm
         private static Queue<string> ToRPN(string expression)
         {
+            //Usually stack used for output and expression is read from the end but it easier for me use queue 
             Queue<string> output = new Queue<string>();
             Stack<string> operatorStack = new Stack<string>();
+            //For conventer
+            expression = expression.Replace(".", ",");
             //To differentiate between unary and binary minus, we will replace unary minus with regex
             expression = Regex.Replace(expression, $"(?<=[({operators}])-", "_");
-            Console.WriteLine(expression);
             //Regex Split, because String.Split() don't include separators
             var tokens = Regex.Split(expression, 
-                $"([){operators}]|{String.Join("|",functions.Select(e=>$"(?:{e})"))})" //Looks like this ([\\+\\-\\*\\/]|(?:f1)|(?:f2))
+                $"([(){operators}]|{String.Join("|",functions.Select(e=>$"(?:{e})"))})" //Looks like this ([\\+\\-\\*\\/]|(?:f1)|(?:f2))
                 );
-            //Main algorithm, read "Shunting yard algorithm" for details
+            //Main algorithm written like in wikipedia, read "Shunting yard algorithm" for details
             foreach (var token in tokens)
             {
                 if (String.IsNullOrEmpty(token)) continue;
@@ -128,6 +161,14 @@ namespace Lesson3
             return output;
         }
 
+        /* calculates result from reverse polish notation 
+         * Algorithm:
+         * while there is a token
+         *   if operand then place in stack
+         *   if operator or function then take nessesary amount for that operation from stack
+         *      and place result in stack
+         * get answer from one remaining element in stack
+         */
         public static double calculateRPN(Queue<string> rpn)
         {
             Stack<double> stack = new Stack<double>();
@@ -135,6 +176,7 @@ namespace Lesson3
             {
                 switch (GetType(elem))
                 {
+                    case RPNType.FUNCTION:
                     case RPNType.OPERATION:
                         double[] args = new double[opInfo[elem].RequiredAmount];
                         for (int i = opInfo[elem].RequiredAmount - 1; i >=0 ; --i)
@@ -157,11 +199,78 @@ namespace Lesson3
             return stack.Pop();
         }
 
+        public static void Help()
+        {
+            Console.Write(
+                "This is calculator based on Reverse Polish Notation\n" +
+                "Write command or any valid expression to get result\n" +
+                "For example 2+4.2*-(7+3^abs(-2))\n" +
+                "Supported operators and functions:\n" +
+                $"    {operators.Replace("\\", "").Replace("_","")} {String.Join(" ",functions)}\n" +
+                $"Commands (if no command given input will be treated as expression):\n"+
+                " help - this message\n" +
+                " quit, exit - exit program\n" +
+                " history [n] - get last n operations (default 5)\n"
+                ) ;
+        }
+        static List<string> historyList = new List<string>();
+
+        public static void History(int amount = 5)
+        {
+            if (amount < 1) throw new ExpectedException("Amount of elements in history must be positive");
+            Console.WriteLine("History:");
+            for (int i = Math.Max(historyList.Count - amount, 0); i < historyList.Count; i++)
+            {
+                Console.WriteLine(historyList[i]);
+            }
+        }
         public static void Main(string[] args)
         {
-            string str = Console.ReadLine();
-            var st = ToRPN(str);
-            Console.WriteLine(calculateRPN(st));
+            bool running = true;
+            Help();
+            while (running)
+            {
+                try
+                {
+                    Console.Write("> ");
+                    string fullLine = Console.ReadLine()?.Trim() ?? "";
+                    string[] command = fullLine.Split(' ');
+                    switch (command[0].ToLower())
+                    {
+                        case "quit":
+                        case "exit":
+                            running = false;
+                            break;
+                        case "help":
+                            Help();
+                            break;
+                        case "history":
+                            if (command.Length > 1)
+                            {
+                                try
+                                {
+                                    History(Convert.ToInt32(command[1]));
+                                }
+                                catch (FormatException) { throw new ExpectedException("History accepts integer number as argument"); }
+                            }
+                            else History();
+                            break;
+                        default:
+                            double result = calculateRPN(ToRPN(fullLine));
+                            Console.WriteLine(result);
+                            historyList.Add($"{fullLine.Replace(" ", "")}={result}");
+                            break;
+                    }
+                }
+                catch (ExpectedException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
         }
     }
 }
